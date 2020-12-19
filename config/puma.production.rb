a96c15c9a5296e6b34dcfd9e08b622586af5cb0e
ts-3156 <ts_3156@yahoo.co.jp>
2020-12-19 09:15:40 +0900

    Change a location of a sock file to avoid PrivateTmp

--- config/puma.production.rb
+++ config/puma.production.rb
@@ -3,8 +3,8 @@ environment 'production'
 dir = Dir.pwd
 
 threads 2, 2
-daemonize true
+# daemonize true
 pidfile "#{dir}/tmp/pids/puma.pid"
 state_path "#{dir}/tmp/pids/puma.state"
-bind 'unix:///tmp/puma.sock'
+bind 'unix:///var/translator/tmp/puma.sock'
 stdout_redirect "#{dir}/log/puma.log", "#{dir}/log/puma.log", true
