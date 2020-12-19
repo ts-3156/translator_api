7168d9717c656811445bd51733cd9001d773236f
ts-3156 <ts_3156@yahoo.co.jp>
2020-12-19 18:03:42 +0900

    Increase the CHARS_PER_TRANSLATION of a trial plan

--- app/lib/limitation.rb
+++ app/lib/limitation.rb
@@ -1,4 +1,4 @@
 class Limitation
-  CHARS_PER_TRANSLATION = { trial: 50, free: 250, pro: 20_000 }
+  CHARS_PER_TRANSLATION = { trial: 100, free: 250, pro: 20_000 }
   TOTAL_CHARS = { trial: 1000, free: 10000, pro: 100_000 }
 end
