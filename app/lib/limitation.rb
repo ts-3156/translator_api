3ca5955d5059ba635b3985a63354e07e8aac64a3
ts-3156 <ts_3156@yahoo.co.jp>
2020-12-21 06:58:44 +0900

    Increase CHARS_PER_TRANSLATION for the free version

--- app/lib/limitation.rb
+++ app/lib/limitation.rb
@@ -1,4 +1,4 @@
 class Limitation
-  CHARS_PER_TRANSLATION = { trial: 100, free: 250, pro: 20_000 }
-  TOTAL_CHARS = { trial: 1000, free: 10000, pro: 100_000 }
+  CHARS_PER_TRANSLATION = { trial: 100, free: 2000, pro: 20_000 }
+  TOTAL_CHARS = { trial: 1000, free: 10_000, pro: 100_000 }
 end
